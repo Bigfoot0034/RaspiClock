92398be9e078e828a9fb8fb40c6b61bcf8954a13
Bigfoot0034 <guillaume.norbert@gmail.com>
2017-05-14 22:46:45 +0200

    Create RaspiClock.py

---
+++ RaspiClock.py
@@ -0,0 +1,26 @@
+import time
+
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.led_matrix.device import max7219
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, LCD_FONT, CP437_FONT, SINCLAIR_FONT, TINY_FONT
+from luma.core import legacy
+
+
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=4, block_orientation=-90)
+
+contrast = 25
+
+while True:
+
+	device.contrast(contrast)
+
+	hour = time.strftime("%H")
+	minutes = time.strftime("%M")
+	aff = hour + ":" + minutes
+
+	with canvas(device) as draw:
+		legacy.text(draw, (1, 1), aff, fill="white", font=proportional(TEST_FONT))
+
